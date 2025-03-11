0f1fa55c30a2195ed0ffbe1fcf198144847a064f
PRAKHAR MADHARIA <155750280+prakhar1304@users.noreply.github.com>
2025-03-11 16:16:28 +0530

    it  shoudl  work now

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 
 import connectDB from "./db/index.js";
-import {app} from "./app.js"
+import { app } from "./app.js"
 import dotenv from "dotenv"
 //dotenv.config(); // No need to specify path if your .env is in the root directory
-dotenv.config({path: "./.env"});
+dotenv.config({ path: "./.env" });
 
+if (process.platform === 'linux') {
+    throw new Error('linux is NOT supported');
+}
 
 //when  a  async  process is ende  it  return a promoise
 connectDB()
-.then(() =>{
-    app.listen(process.env.PORT || 8000 ,()=>{
-        console.log(`port is ruunung ${process.env.PORT}`);
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`port is ruunung ${process.env.PORT}`);
+        })
     })
-})
-.catch((err) => {
-    console.log(`Moogodb connection failed-- ,  ${err}`)
-})
+    .catch((err) => {
+        console.log(`Moogodb connection failed-- ,  ${err}`)
+    })
 //callback   are  executed  when  called 
